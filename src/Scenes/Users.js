97666b903a65cc17c1822adaf4d07b06445bf0fe
nicole-allen679 <nicole.allen679@gmail.com>
2021-05-18 17:07:55 -0400

    got get and post working, created form for newUser

--- src/Scenes/Users.js
+++ src/Scenes/Users.js
@@ -1,38 +1,56 @@
 import React, { useEffect, useState } from 'react'
+import { Button } from 'react-materialize'
 
 function Users() {
   const [allUsers, setAllUsers] = useState(null)
-  //const [newUser, setNewUser] = useState({})
+  const [newUser, setNewUser] = useState({})
   useEffect(() => {
     fetch('http://localhost:5000/users')
       .then((response) => response.json())
       .then((data) => setAllUsers(data))
       .catch((err) => console.log('This is error', err))
   }, [])
-  console.log(allUsers)
+
+  function createUser() {
+    fetch('http://localhost:/5000/signup', {
+      method: 'Post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+  }
   return (
-      <div className="container">
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4>First Names</h4>
-      </li>
-      {allUsers && allUsers.map((user) => {
-        return (
-          <li className="collection-item">
-            <div>
-              {user.email}
-              <a href="#!" className="secondary-content">
-                <i className="material-icons">send</i>
-              </a>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
-    <label>Email</label>
-    <input onChange=""/>
-    <label>Password</label>
-    <input />
+    <div className="container">
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4>First Names</h4>
+        </li>
+        {allUsers &&
+          allUsers.map((user) => {
+            return (
+              <li className="collection-item">
+                <div>
+                  {user.email}
+                  <a href="#!" className="secondary-content">
+                    <i className="material-icons">send</i>
+                  </a>
+                </div>
+              </li>
+            )
+          })}
+      </ul>
+      <label>Email</label>
+      <input
+        onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+      />
+      <label>Password</label>
+      <input
+        onChange={(e) => setNewUser({ ...newUser, password: e.target.value })}
+      />
+      <Button onClick={() => createUser()}>Create User</Button>
+      <br />
     </div>
   )
 }
